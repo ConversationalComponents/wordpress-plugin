4713269e0123f466354c11e21f4b02748128c901
AlexTavor <alex@imperson.com>
2020-02-13 18:52:57 +0200

    Now with readme, taking params as attributes from erw-root

---
+++ README.md
@@ -0,0 +1,24 @@
+## CoCo WordPress ChatWindow Plugin
+
+##### ChatWindow API:
+
+Chat window is added to a DOM element called "erw-root"
+
+It accepts the following parameters:
+
+```js
+{
+    name: string; // name to appear in header
+    humanIdOrUrl: string; // full url for exchange or ID of bot
+    botGreeting?: string; // what the bot should say before chat begins, defaults to "Type anything to get started!"
+    isFabless?: boolean; // when false/undefined, window is attached to a button, fixed in lower right corner. Otherwise it'll be placed under the root component
+    height?: number; // desired height, defaults to 500
+    width?: number; // desired width, defaults to 300
+}
+```
+
+Parameters are taken from attributes of "erw-root". Example:
+
+```html
+<div id="erw-root" name="Namer" humanIdOrUrl="namer_vp3"></div>
+```
---
+++ coco-bot/build/precache-manifest.da259c24852dccd14ca97657ffcdc92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea17e15d3205a658ee85bedf133381",
+    "url": "/wp-content/plugins/coco-bot/build/index.html"
+  },
+  {
+    "revision": "f7282fe4f0292da60176",
+    "url": "/wp-content/plugins/coco-bot/build/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "c384c3cee3a93072fde1",
+    "url": "/wp-content/plugins/coco-bot/build/static/js/2.6abde3b6.chunk.js"
+  },
+  {
+    "revision": "a33639c6c7aec196102819643b70c233",
+    "url": "/wp-content/plugins/coco-bot/build/static/js/2.6abde3b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7282fe4f0292da60176",
+    "url": "/wp-content/plugins/coco-bot/build/static/js/main.2986d1ff.chunk.js"
+  },
+  {
+    "revision": "fec6fa22ec0f8bc9ff2f",
+    "url": "/wp-content/plugins/coco-bot/build/static/js/runtime-main.0cb38e19.js"
+  }
+]);

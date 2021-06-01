ad32702e60e50782e55c40e8b4aedf217e094358
VictorCoCo <victor@a-i.com>
2021-06-01 15:33:17 +0300

    Change human ID to channel ID

--- cocohub/includes/shortcode.php
+++ cocohub/includes/shortcode.php
@@ -16,7 +16,7 @@ add_shortcode( 'cocobot', function ( $atts ) {
 	$params       .= ( ! empty( $coco_bot['name_0'] ) ) ? 'bot_name="'.$coco_bot['name_0'].'"' : 'bot_name="CoCoHub"';
 	$params       .= ( ! empty( $coco_bot['source_language_code_7'] ) ) ? 'source_language_code="'.$coco_bot['source_language_code_7'].'"' : 'source_language_code=""';
 	$params       .= ( ! empty( $coco_bot['user_own_email_field'] ) ) ? 'user_email="'.$coco_bot['user_own_email_field'].'"' : 'user_email=""';
-	$params       .= ( ! empty( $component ) ) ? 'human_id_or_url="'.$component.'"' : ( ( ! empty( $coco_bot['human_id_or_url'] ) ) ? 'human_id_or_url="'.$coco_bot['human_id_or_url'].'"' : 'human_id_or_url="coco_bot_vp3"' );
+	$params       .= ( ! empty( $channel_id ) ) ? 'channel_id="'.$channel_id.'"' : ( ( ! empty( $coco_bot['channel_id'] ) ) ? 'channel_id="'.$coco_bot['channel_id'].'"' : 'channel_id=""' );
 	$params       .= ( ! empty( $coco_bot['botgreeting_2'] ) ) ? 'bot_greeting="'.$coco_bot['botgreeting_2'].'"' : 'bot_greeting="Hello"';
 	$params       .= ( ! empty( $coco_bot['isfabless_3'] ) ) ? 'is_fabless="'.$coco_bot['isfabless_3'].'"' : 'is_fabless=""';
 	$params       .= ( ! empty( $coco_bot['isrtl_3'] ) ) ? 'is_rtl="'.$coco_bot['isrtl_3'].'"' : 'is_rtl=""';

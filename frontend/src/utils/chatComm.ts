003ae4f8d57ccf4befee954733c6c476fa545a66
VictorCoCo <victor@a-i.com>
2021-06-02 17:21:58 +0300

    Add send message to comp message.

--- frontend/src/utils/chatComm.ts
+++ frontend/src/utils/chatComm.ts
@@ -61,3 +61,59 @@ export const sendMessage: (p: {
     return { error: e };
   }
 };
+
+export const sendMessageComponent: (p: {
+  componentIdOrUrl: string;
+  message: string;
+  inputParameters: ComponentProperty[];
+  componentName: string;
+  newSessionId?: string;
+  source_language_code?: string;
+  user_email?: string;
+}) => Promise<any> = async ({
+  componentIdOrUrl,
+  message,
+  inputParameters,
+  newSessionId,
+  componentName,
+  source_language_code,
+  user_email,
+}) => {
+  const headers = new Headers({ "api-key": apiKey });
+  if (newSessionId) {
+    sessionId = newSessionId;
+  }
+  try {
+    const isUrl =
+      componentIdOrUrl.startsWith("http:") ||
+      componentIdOrUrl.startsWith("https:");
+    const reply = await fetch(
+      isUrl
+        ? `${componentIdOrUrl}/${sessionId}`
+        : url + `/api/exchange/${componentIdOrUrl}/${sessionId}`,
+      {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+          source_language_code,
+          user_input: message,
+          flatten_context: true,
+          context: {
+            bot_name: componentName,
+            owner: { email: user_email },
+            ...inputParameters.reduce((acc, cur) => {
+              // @ts-ignore
+              acc[cur.name] = cur.value;
+              return acc;
+            }, {}),
+          },
+        }),
+      }
+    );
+    const json = await reply.json();
+    return json;
+  } catch (e) {
+    console.error(e);
+    return { error: e };
+  }
+};
